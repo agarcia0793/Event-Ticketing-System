fe88e0e30eb90078581cfe10851ad710aae78ce7
Adriana Garcia <123328876+agarcia0793@users.noreply.github.com>
2025-05-09 22:08:34 -0500

    Update bookings.js

--- routes/bookings.js
+++ routes/bookings.js
@@ -2,15 +2,18 @@ import express from 'express';
 import {
   getUserBookings,
   getBookingById,
-  createBooking
+  createBooking,
+  validateBookingQR
 } from '../controllers/bookingController.js';
-import { protect } from '../middlewares/auth.js';
+import auth from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.use(protect);
+router.use(auth);
+
 router.get('/', getUserBookings);
 router.get('/:id', getBookingById);
 router.post('/', createBooking);
+router.get('/validate/:qr', validateBookingQR);
 
 export default router;
